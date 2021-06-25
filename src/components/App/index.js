49a64829aca05411410ff11878b1001ca6167987
wahoostik <anthonypaccaud@wanadoo.fr>
2021-06-25 18:25:43 +0200

    component App

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import './style.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p>
+          Portfolio
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
